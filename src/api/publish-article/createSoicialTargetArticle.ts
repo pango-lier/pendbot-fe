b545e78f823a43de8f2563d2ca973fc17493d91d
trong <binhtrongcdt1@gmail.com>
2025-01-13 16:56:26 +0700

    w

---
+++ src/api/publish-article/createSoicialTargetArticle.ts
@@ -0,0 +1,16 @@
+import { IUpdateArticle } from "api/articles/type/type.interface";
+import { hookApi } from "api/hookApi";
+import { ISocialTarget } from "api/socialTargets/type/type.interface";
+import axios from "axios";
+export interface ICreate {
+  socialTargets: ISocialTarget[];
+  articles?: IUpdateArticle[];
+}
+
+export const createProxy = async (params: ICreate) => {
+  return await hookApi("post", `publish-socials/articles`, {
+    params,
+    _success: true,
+    title: "Publish Article to Social",
+  });
+};
